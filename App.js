3e5c338c47e1f52577a1a89484dcca9b69f925a1
landeilsonveloso <landeilsonveloso@gmail.com>
2024-03-14 00:35:22 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react'
+import { Button, Image, Text, TextInput, View } from 'react-native'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [item, setItem] = useState("")
+  const [visibleItem, setVisibleItem] = useState(item)
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+    <View style={{flex: 1, flexDirection: "column", backgroundColor: "#eee", marginTop: 30}}>
+      <View style={{flex: 4, justifyContent: "center", alignItems: "center",}}>
+        <Image style={{width: 250, height: 250}} source={require("./assets/bag.jpg")}/>
+      </View>
+
+      <View style={{flex: 2 , flexDirection: "column", alignItems: "center", justifyContent: "space-evenly"}}>
+        <View>
+          <Text>CADASTRE SEU ITEM ESCOLAR</Text>
+        </View>
+
+        <View style={{flexDirection: "row", justifyContent: "center", alignItems: "center"}}>
+          <Text>ITEM:</Text>
+          <TextInput style={{width: 225, height: 30, borderWidth: 1, marginLeft: 5, padding: 5}} autoCapitalize='characters' value={item} onChangeText={newItem => setItem(newItem)}/>
+        </View>
+      </View> 
+
+      <View style={{flex: 1, flexDirection: "row", justifyContent: "space-around", alignItems: "center"}}>
+        <View>
+          <Button title="Limpar" color="red" onPress={() => setItem("")}/>
+        </View>
+
+        <View>
+          <Button title="Cadastrar" onPress={() => setVisibleItem(item)}/>
+        </View>
+      </View>
+
+      <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+        <Text>{visibleItem}</Text>
+      </View>
+    </View>
+  )
+}
